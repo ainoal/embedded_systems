ff05eb09c53d33251a71b9cf250f3a2ddbe71a7d
ainoal <77739788+ainoal@users.noreply.github.com>
2023-04-06 21:45:52 +0300

    keypad test works

--- keypad_test/KeypadTest/KeypadTest/main.c
+++ keypad_test/KeypadTest/KeypadTest/main.c
@@ -18,46 +18,40 @@
 int main(void)
 {
     /********************testing****************************/
-    char input;
-    FILE uart_output = FDEV_SETUP_STREAM(USART_transmit, NULL, _FDEV_SETUP_WRITE);
-    FILE uart_input = FDEV_SETUP_STREAM(NULL, USART_receive, _FDEV_SETUP_READ);
-    stdout = &uart_output;
-    stdin = &uart_input;
-    
     DDRB |= (1 << PB0);
     DDRB |= (1 << PB1);
     DDRB |= (1 << PB2);
     DDRB |= (1 << PB3);
     /******************************************************/
     
-    // Set digital pins 0-3 (columns) as input and 4-7 (rows) as output
-    DDRD |= 0b11110000;
+    // Set digital pins 0-3 (rows) as output and 4-7 (columns) as input
+    DDRD |= 0b00001111;
     _delay_ms(1);
     
-    PORTD |= 0b00001111;    // Power the row pins
+    PORTD = 0b11110000;    // Power the row pins
     
     int key_pressed = 0;
     
     while (1) 
     {
-        PORTB &= ~(1 << PB0);
+        /*PORTB &= ~(1 << PB0);
         _delay_ms(1000);
         PORTB |= (1 << PB0);
         _delay_ms(1000);
         PORTB &= ~(1 << PB0);
-        _delay_ms(1000);
+        _delay_ms(1000);*/
         
-        if (PIND!= 0b00001111) {     // If any of column pins goes high
+        if (PIND != 0b11110000) {     // If any of row pins goes low (!??)
             key_pressed = PIND;
-            DDRD ^= 0b11111111;      // Make rows as inputs and columns as output
+            _delay_ms(10);
+            DDRD ^= 0b11111111;      // Make rows as inputs and columns as outputs
             _delay_ms(1);
-             PORTD ^= 0b11111111;   // Power the columns
-            _delay_ms(1);
-
+             PORTD ^= 0b11111111;        // Power the columns
+            _delay_ms(100);
             
-            key_pressed |= PIND;    // The variable has now both row and column values
+            key_pressed |= PIND;    // The variable has now both row and column values as 0, others as 1
             
-            if (key_pressed == 0b00010001) {
+            if (key_pressed == 0b01110111) {
                 // Key 1 pressed
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -65,7 +59,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-            } else if (key_pressed == 0b00100001) {
+            } 
+            else if (key_pressed == 0b01111011) {
                 // Key 2 pressed
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -73,12 +68,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                
-                puts("2");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b01000001) {
+            } 
+            else if (key_pressed == 0b01111101) {
                 // Key 3 pressed
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -86,11 +77,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("3");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b10000001) {
+            } 
+            else if (key_pressed == 0b01111110) {
                 // A
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -98,11 +86,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("A");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b00010010) {
+            } 
+            else if (key_pressed == 0b10110111) {
                 // 4
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -110,11 +95,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("4");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b00100010) {
+            } 
+            else if (key_pressed == 0b10111011) {
                 // 5
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -122,11 +104,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("5");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b01000010) {
+            } 
+            else if (key_pressed == 0b10111101) {
                 // 6
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -134,9 +113,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                //puts("6");
-            } else if (key_pressed == 0b10000010) {
+            } 
+            else if (key_pressed == 0b10111110) {
                 // B
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -144,11 +122,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("B");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b00010100) {
+            } 
+            else if (key_pressed == 0b11010111) {
                 // 7
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -156,11 +131,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("7");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b00100100) {
+            } 
+            else if (key_pressed == 0b11011011) {
                 // 8
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -168,11 +140,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("8");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b01000100) {
+            } 
+            else if (key_pressed == 0b11011101) {
                 // 9
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -180,11 +149,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("9");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b10000100) {
+            } 
+            else if (key_pressed == 0b11011110) {
                 // C
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -192,11 +158,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("C");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b00011000) {
+            } 
+            else if (key_pressed == 0b11100111) {
                 // *
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -204,9 +167,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("*");
-            } else if (key_pressed == 0b00101000) {
+            } 
+            else if (key_pressed == 0b11101011) {
                 // 0
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -214,11 +176,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("0");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b01001000) {
+            } 
+            else if (key_pressed == 0b11101101) {
                 // #
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -226,11 +185,8 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("#");
-                input = getchar();
-                printf("%c\n", input);
-            } else if (key_pressed == 0b10001000) {
+            } 
+            else if (key_pressed == 0b11101110) {
                 // D
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
@@ -238,10 +194,6 @@ int main(void)
                 _delay_ms(1000);
                 PORTB &= ~(1 << PB0);
                 _delay_ms(1000);
-                
-                puts("D");
-                input = getchar();
-                printf("%c\n", input);
             }
             
             DDRD ^= 0b11111111;
