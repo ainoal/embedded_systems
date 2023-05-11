e366dc0a9f10941bd7181e5e5529ab02386d8011
ainoal <77739788+ainoal@users.noreply.github.com>
2023-05-11 21:22:09 +0300

    Print keaypad numbers, stops after a few pressed keys

--- keypad_test/KeypadTest/KeypadTest/main.c
+++ keypad_test/KeypadTest/KeypadTest/main.c
@@ -85,10 +85,10 @@ int main(void)
         
         //printf("Keypad_vals %d", keypad_vals);
         keypad_vals &= 0b00001111;
-        printf("kp_vals: %d  portd_vals: %d  portb_vals: %d  PINB: %d\n\r", keypad_vals, portd_vals, portb_vals, PINB);
+        //printf("kp_vals: %d  portd_vals: %d  portb_vals: %d  PINB: %d\n\r", keypad_vals, portd_vals, portb_vals, PINB);
         
         if (keypad_vals != 0b00001111) {     // If any of column pins goes high
-            printf("if\n\r");
+            //printf("if\n\r");
             key_pressed = keypad_vals;
             //_delay_ms(1);
    
@@ -103,26 +103,26 @@ int main(void)
             PORTD &= 0b00111111;
             _delay_ms(1);
             
-            printf("PINB %d   ", PINB);
+            //printf("PINB %d   ", PINB);
             portd_vals = ((PIND & BITMASK_D) >> 6);
             portb_vals = ((PINB & BITMASK_B) << 2);
-            printf("portb_vals %d   portd_vals %d ", portb_vals, portd_vals);
+            //printf("portb_vals %d   portd_vals %d ", portb_vals, portd_vals);
             
             //_delay_ms(10);
             keypad_vals = portd_vals | portb_vals;
             keypad_vals &= 0b11110000;
             
             //printf("PINB %d   ", PINB);
-            printf("Key pressed %d   ", key_pressed);
+            //printf("Key pressed %d   ", key_pressed);
             //_delay_ms(10);
             key_pressed |= keypad_vals;    // The variable has now both row and column values
-            printf("%d\n\r", key_pressed);
+            //printf("%d\n\r", key_pressed);
             
             if (key_pressed == 0b01110111) {
                 // Key 1 pressed
+                printf("1");
                 given_password[idx] = '1';
                 if (password[idx] == '1') {
-                    led_test();
                 }
                 else {
                     ;
@@ -130,9 +130,9 @@ int main(void)
             }
             else if (key_pressed == 0b01111011) {
                 // Key 2 pressed
+                printf("2");
                 given_password[idx] = '2';
                 if (password[idx] == '2') {
-                    led_test();
                 }
                 else {
                     ;
@@ -140,9 +140,9 @@ int main(void)
             }
             else if (key_pressed == 0b01111101) {
                 // Key 3 pressed
+                printf("3");
                 given_password[idx] = '3';
                 if (password[idx] == '3') {
-                    led_test();
                 }
                 else {
                     ;
@@ -150,9 +150,9 @@ int main(void)
             }
             else if (key_pressed == 0b01111110) {
                 // A
+                printf("A");
                 given_password[idx] = 'A';
                 if (password[idx] == 'A') {
-                    led_test();
                 }
                 else {
                     ;
@@ -160,9 +160,9 @@ int main(void)
             }
             else if (key_pressed == 0b10110111) {
                 // 4
+                printf("4");
                 given_password[idx] = '4';
                 if (password[idx] == '4') {
-                    led_test();
                 }
                 else {
                     ;
@@ -170,9 +170,9 @@ int main(void)
             }
             else if (key_pressed == 0b10111011) {
                 // 5
+                printf("5");
                 given_password[idx] = '5';
                 if (password[idx] == '5') {
-                    led_test();
                 }
                 else {
                     ;
@@ -180,9 +180,9 @@ int main(void)
             }
             else if (key_pressed == 0b10111101) {
                 // 6
+                printf("6");
                 given_password[idx] = '6';
                 if (password[idx] == '6') {
-                    led_test();
                 }
                 else {
                     ;
@@ -190,9 +190,9 @@ int main(void)
             }
             else if (key_pressed == 0b10111110) {
                 // B
+                printf("B");
                 given_password[idx] = 'B';
                 if (password[idx] == 'B') {
-                    led_test();
                 }
                 else {
                     ;
@@ -200,9 +200,9 @@ int main(void)
             }
             else if (key_pressed == 0b11010111) {
                 // 7
+                printf("7");
                 given_password[idx] = '7';
                 if (password[idx] == '7') {
-                    led_test();
                 }
                 else {
                     ;
@@ -210,9 +210,9 @@ int main(void)
             }
             else if (key_pressed == 0b11011011) {
                 // 8
+                printf("8");
                 given_password[idx] = '8';
                 if (password[idx] == '8') {
-                    led_test();
                 }
                 else {
                     ;
@@ -220,9 +220,9 @@ int main(void)
             }
             else if (key_pressed == 0b11011101) {
                 // 9
+                printf("9");
                 given_password[idx] = '9';
                 if (password[idx] == '9') {
-                    led_test();
                 }
                 else {
                     ;
@@ -230,9 +230,9 @@ int main(void)
             }
             else if (key_pressed == 0b11011110) {
                 // C
+                printf("C");
                 given_password[idx] = 'C';
                 if (password[idx] == 'C') {
-                    led_test();
                 }
                 else {
                     ;
@@ -240,10 +240,10 @@ int main(void)
             }
             else if (key_pressed == 0b11100111) {
                 // *
+                printf("*");
                 // TODO: make this the backspace button
                 given_password[idx] = '*';
                 if (password[idx] == '*') {
-                    led_test();
                 }
                 else {
                     ;
@@ -251,9 +251,9 @@ int main(void)
             }
             else if (key_pressed == 0b11101011) {
                 // 0
+                printf("0");
                 given_password[idx] = '0';
                 if (password[idx] == '0') {
-                    led_test();
                 }
                 else {
                     ;
@@ -261,6 +261,7 @@ int main(void)
             }
             else if (key_pressed == 0b11101101) {
                 // #
+                printf("#");
                 compare(password, given_password);
                 //printf("#");
                 //given_password[idx] = '#';
@@ -268,9 +269,9 @@ int main(void)
             }
             else if (key_pressed == 0b11101110) {
                 // D
+                printf("D");
                 given_password[idx] = 'D';
                 if (password[idx] == 'D') {
-                    led_test();
                 }
                 else {
                     ;
